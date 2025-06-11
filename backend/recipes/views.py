21b1250e76ffb1ab024208e8d035e85dca49fbd5
whyourelated <whyourelated@gmail.com>
2025-06-11 18:07:28 +0700

    Исправлены замечания

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import redirect
 from .models import Recipe
 
 def short_recipe_redirect(request, pk):
-    if not Recipe.objects.filter(pk=pk).exists():
-        raise Http404('Рецепт не найден.')
-    return redirect('recipes:recipe_detail', recipe_id=pk)
+    try:
+        recipe = Recipe.objects.get(pk=pk)
+    except Recipe.DoesNotExist:
+        raise Http404(f'Рецепт с id {pk} не найден.')
+    
+    return redirect(f'/recipes/{pk}/')
